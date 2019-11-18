a860144f0ab4402ce21f90e4121566b878c9bd3f
WallaceMoura <wallace.moura2015@gmail.com>
2019-11-18 20:06:55 -0300

    Criação do design de todos os Formulários

--- Sieve.Desktop/Sieve/Janelas/Formularios/ClienteForm.Designer.cs
+++ Sieve.Desktop/Sieve/Janelas/Formularios/ClienteForm.Designer.cs
@@ -28,12 +28,204 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.textBox10 = new System.Windows.Forms.TextBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.textBox12 = new System.Windows.Forms.TextBox();
+            this.textBox13 = new System.Windows.Forms.TextBox();
+            this.textBox14 = new System.Windows.Forms.TextBox();
+            this.textBox15 = new System.Windows.Forms.TextBox();
+            this.textBox16 = new System.Windows.Forms.TextBox();
+            this.textBox17 = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.comboBox3);
+            this.groupBox1.Controls.Add(this.textBox10);
+            this.groupBox1.Controls.Add(this.comboBox2);
+            this.groupBox1.Controls.Add(this.textBox9);
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Controls.Add(this.textBox5);
+            this.groupBox1.Controls.Add(this.textBox4);
+            this.groupBox1.Controls.Add(this.textBox3);
+            this.groupBox1.Location = new System.Drawing.Point(12, 132);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(478, 132);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(78, 78);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(255, 21);
+            this.comboBox3.TabIndex = 17;
+            // 
+            // textBox10
+            // 
+            this.textBox10.Location = new System.Drawing.Point(339, 78);
+            this.textBox10.Name = "textBox10";
+            this.textBox10.Size = new System.Drawing.Size(122, 20);
+            this.textBox10.TabIndex = 16;
+            this.textBox10.Text = "Complemento";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(194, 51);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(267, 21);
+            this.comboBox2.TabIndex = 15;
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(14, 52);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(174, 20);
+            this.textBox9.TabIndex = 14;
+            this.textBox9.Text = "BAIRRO";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(14, 78);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(58, 21);
+            this.comboBox1.TabIndex = 13;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(14, 26);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 12;
+            this.textBox5.Text = "CEP";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(120, 26);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(277, 20);
+            this.textBox4.TabIndex = 11;
+            this.textBox4.Text = "Endereço";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(403, 26);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(58, 20);
+            this.textBox3.TabIndex = 10;
+            this.textBox3.Text = "Nº";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.textBox12);
+            this.groupBox2.Controls.Add(this.textBox13);
+            this.groupBox2.Controls.Add(this.textBox14);
+            this.groupBox2.Controls.Add(this.textBox15);
+            this.groupBox2.Controls.Add(this.textBox16);
+            this.groupBox2.Controls.Add(this.textBox17);
+            this.groupBox2.Location = new System.Drawing.Point(12, 23);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(480, 103);
+            this.groupBox2.TabIndex = 11;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "groupBox2";
+            // 
+            // textBox12
+            // 
+            this.textBox12.Location = new System.Drawing.Point(173, 71);
+            this.textBox12.Name = "textBox12";
+            this.textBox12.Size = new System.Drawing.Size(91, 20);
+            this.textBox12.TabIndex = 15;
+            this.textBox12.Text = "DataNasc";
+            // 
+            // textBox13
+            // 
+            this.textBox13.Location = new System.Drawing.Point(14, 45);
+            this.textBox13.Name = "textBox13";
+            this.textBox13.Size = new System.Drawing.Size(153, 20);
+            this.textBox13.TabIndex = 14;
+            this.textBox13.Text = "CPF";
+            // 
+            // textBox14
+            // 
+            this.textBox14.Location = new System.Drawing.Point(173, 45);
+            this.textBox14.Name = "textBox14";
+            this.textBox14.Size = new System.Drawing.Size(301, 20);
+            this.textBox14.TabIndex = 13;
+            this.textBox14.Text = "EMAIL";
+            // 
+            // textBox15
+            // 
+            this.textBox15.Location = new System.Drawing.Point(14, 71);
+            this.textBox15.Name = "textBox15";
+            this.textBox15.Size = new System.Drawing.Size(153, 20);
+            this.textBox15.TabIndex = 12;
+            this.textBox15.Text = "TELEFONE";
+            // 
+            // textBox16
+            // 
+            this.textBox16.Location = new System.Drawing.Point(173, 19);
+            this.textBox16.Name = "textBox16";
+            this.textBox16.Size = new System.Drawing.Size(301, 20);
+            this.textBox16.TabIndex = 11;
+            this.textBox16.Text = "Sobrenome";
+            // 
+            // textBox17
+            // 
+            this.textBox17.Location = new System.Drawing.Point(14, 19);
+            this.textBox17.Name = "textBox17";
+            this.textBox17.Size = new System.Drawing.Size(153, 20);
+            this.textBox17.TabIndex = 10;
+            this.textBox17.Text = "Nome";
+            // 
+            // ClienteForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(502, 276);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "ClienteForm";
             this.Text = "ClienteForm";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.TextBox textBox10;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox textBox12;
+        private System.Windows.Forms.TextBox textBox13;
+        private System.Windows.Forms.TextBox textBox14;
+        private System.Windows.Forms.TextBox textBox15;
+        private System.Windows.Forms.TextBox textBox16;
+        private System.Windows.Forms.TextBox textBox17;
     }
 }
--- Sieve.Desktop/Sieve/Janelas/Formularios/FornecedorForm.cs
+++ Sieve.Desktop/Sieve/Janelas/Formularios/FornecedorForm.cs
@@ -16,5 +16,7 @@ namespace Sieve.Janelas.Formularios
         {
             InitializeComponent();
         }
+
+       
     }
 }
