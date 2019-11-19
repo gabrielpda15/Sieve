a68febc2a25d6d0122c065ec80c8bb5f44aaecbd
gabrielpda15 <gabriel.pda15@gmail.com>
2019-11-19 13:46:47 -0300

    Inclusão das entidades de localização

---
+++ Sieve.Desktop/Sieve.Models/Location/Country.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sieve.Models.Location
+{
+    public class Country : Base.BaseEntity
+    {
+        public string Name { get; set; }
+        public string Code { get; set; }
+        public virtual IList<Region> Regions { get; set; }
+    }
+}
---
+++ Sieve.Desktop/Sieve.Models/Location/Region.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sieve.Models.Location
+{
+    public class Region : Base.BaseEntity
+    {
+        public string Name { get; set; }
+        public virtual int CountryId { get; set; }
+        public virtual IList<City> Cities { get; set; }
+    }
+}
