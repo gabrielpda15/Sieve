25d1a3ddc55ec68b8ba32f81202c9dc33aaf3155
gabrielpda15 <gabriel.pda15@gmail.com>
2019-11-19 13:49:57 -0300

    Inclusão de extenções

---
+++ Sieve.Desktop/Sieve.Models/Extensions/Mapper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sieve.Models.Extensions
+{
+    internal static class Mapper
+    {
+        public static TOutput Map<TInput, TOutput>(this TInput input, Func<TInput, TOutput> mapper)
+        {
+            return mapper(input);
+        }
+    }
+}
---
+++ Sieve.Desktop/Sieve.Models/Extensions/StringExtension.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sieve.Models.Extensions
+{
+    public static class StringExtension
+    {
+        public static bool IsNothing(this string str)
+        {
+            return string.IsNullOrEmpty(str);
+        }
+
+        public static string[] Split(this string str, string separator, StringSplitOptions options = StringSplitOptions.None)
+        {
+            return str.Split(new string[] { separator }, options);
+        }
+    }
+}
